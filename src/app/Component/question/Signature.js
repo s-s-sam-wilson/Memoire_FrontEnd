83e379ba40794a9d0c7c64c9835a107323ece904
sam_wilson_s_s <83699105+s-s-sam-wilson@users.noreply.github.com>
2025-04-05 09:10:55 +0530

    Update Signature.js

--- src/app/Component/question/Signature.js
+++ src/app/Component/question/Signature.js
@@ -27,7 +27,7 @@ export default function Signature({ questiontext, is_required, preview = false, 
 
   return (
     <div className="mb-6 px-2 sm:px-0">
-      <p className={`font-semibold text-lg sm:text-xl flex items-center mb-2 ${pacifico.className}`}>
+      <p className={`text-gray-900 font-semibold text-lg sm:text-xl flex items-center mb-2 ${pacifico.className}`}>
         {questiontext} {is_required && <span className="text-red-500 text-lg sm:text-xl">*</span>}
       </p>
       <div className="relative bg-white p-4 rounded-lg shadow-md border border-gray-200 hover:shadow-lg transition duration-300">
@@ -36,7 +36,7 @@ export default function Signature({ questiontext, is_required, preview = false, 
           accept="image/*"
           onChange={handleChange}
           disabled={preview}
-          className="w-full border border-gray-300 rounded-lg p-3 text-base sm:text-lg 
+          className="text-gray-900 w-full border border-gray-300 rounded-lg p-3 text-base sm:text-lg 
                      shadow-sm file:mr-4 file:py-2 file:px-4 file:rounded-lg 
                      file:border-0 file:text-sm file:bg-pink-100 file:text-pink-700 
                      hover:file:bg-pink-200 transition-all duration-200 
