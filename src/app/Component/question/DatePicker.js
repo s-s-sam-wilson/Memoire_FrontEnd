a6fc9cd28c0ffa9a25281855c9fe8faf15bb5e3d
sam_wilson_s_s <83699105+s-s-sam-wilson@users.noreply.github.com>
2025-04-05 09:02:44 +0530

    Update DatePicker.js

--- src/app/Component/question/DatePicker.js
+++ src/app/Component/question/DatePicker.js
@@ -20,7 +20,7 @@ export default function DatePicker({ questiontext, is_required, preview = false,
 
   return (
     <div className="mb-6 px-2 sm:px-0">
-      <p className={`font-semibold text-lg sm:text-xl flex items-center mb-2 ${pacifico.className}`}>
+      <p className={`text-gray-900 font-semibold text-lg sm:text-xl flex items-center mb-2 ${pacifico.className}`}>
         {questiontext} {is_required && <span className="text-red-500 text-lg sm:text-xl">*</span>}
       </p>
       <div className="relative bg-white p-4 rounded-lg shadow-md border border-gray-200 hover:shadow-lg transition duration-300">
